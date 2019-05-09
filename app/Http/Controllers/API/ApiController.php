31c4de3be33442d2d678441133865af121b4d68c
Mark Loreto <markyctrigger27@gmail.com>
2019-05-09 21:22:18 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -206,7 +206,7 @@ class ApiController extends BaseController
         ]);
 
         $response = $client->post($endPoint,
-            ['body' => $body]
+            ['body' => json_encode(json_decode($body, true))]
         );
 
         print_r($response);
