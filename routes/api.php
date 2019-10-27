898a8ec4620fd0144871e35e72e875f86cae33a5
Mark Loreto <markyctrigger27@gmail.com>
2019-10-27 21:08:14 +0800

    ...

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,7 @@ Route::middleware('auth:api')->group( function () {
 	Route::get('/getDepot', 'API\ApiController@getDepot');
 	Route::get('/checkKey', 'API\ApiController@checkKey');
 	Route::get('/getRoles', 'API\ApiController@getRoles');
-	Route::get('/getDealersQuickList', 'API\ApiController@getDealersQuickList');
-	Route::post('/getDealersQuickList', 'API\ApiController@getDealersQuickList');
+	Route::match(['get', 'post'], '/getDealersQuickList', 'API\ApiController@getDealersQuickList');
 	Route::get('/getProductCategories', 'API\ApiController@getProductCategories');
 	Route::post('/syncPull', 'API\ApiController@syncPull');
 	Route::post('/syncPullDealers', 'API\ApiController@syncPullDealers');
