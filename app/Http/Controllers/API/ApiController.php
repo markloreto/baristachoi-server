1baf0beaed8007ae5b65adefc086cb2e4d122aa5
Mark Loreto <markyctrigger27@gmail.com>
2019-11-01 01:26:07 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -62,7 +62,11 @@ class ApiController extends BaseController
         $expDate = Carbon::now()->subDays(30);
         foreach($status AS $value){
             if($value == "Prospect"){
-                $machineFilter->where('client_id', '<>', '', 'and');
+                $machineFilter->orWhere('client_id', '<>', '', 'and');
+                //$machineFilter->whereRaw('DATEDIFF(exp_date, current_date) < 31');
+            }
+            if($value == "Lead"){
+                $machineFilter->orWhere('client_id', 'IS NULL', null, 'and');
                 //$machineFilter->whereRaw('DATEDIFF(exp_date, current_date) < 31');
             }
         }
