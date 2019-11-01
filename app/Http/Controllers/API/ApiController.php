8a57949bb8a6ff433183f9627be5d065d9506cd6
Mark Loreto <markyctrigger27@gmail.com>
2019-11-01 09:18:35 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -71,7 +71,7 @@ class ApiController extends BaseController
 
             if(in_array("Active", $status)){
                 $machineFilter = $machineFilter->addSelect(DB::raw('(SELECT COUNT(*) FROM callsheets cs WHERE cs.machine_id = m.id AND cs.name = "Sale") as totalSalesCallsheets'));
-                $machineFilter->whereRaw('DATEDIFF('. $expDate .', (SELECT created_at FROM callsheets WHERE callsheets.machine_id = m.id ORDER BY id DESC LIMIT 1)) < 31');
+                $machineFilter->whereRaw('DATEDIFF("'. $expDate .'", (SELECT created_at FROM callsheets WHERE callsheets.machine_id = m.id ORDER BY id DESC LIMIT 1)) < 31');
             }
 
             $machineFilter->where(function ($query) use ($status) {
