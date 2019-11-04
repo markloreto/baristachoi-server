d305055d68e3aca5944ac1ee8caa0b860705cd40
Mark Loreto <markyctrigger27@gmail.com>
2019-11-05 03:38:51 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -29,8 +29,15 @@ class ApiController extends BaseController
     }
 
     public function fixNoLocations(){
-        $loc = DB::table("locations")->select(DB::raw("region, province, name_2 AS municipal, name_3 AS brgy"))->whereRaw("MbrWithin(GeomFromText(?), shape)", ['POINT(125.517837 7.060798)'])->first();
-        $noLoc = DB::table("machines")->whereRaw('lat IS NOT NULL AND region IS NULL')->get();
+        
+        $noLocs = DB::table("machines")->whereRaw('lat IS NOT NULL AND region IS NULL')->get();
+
+        foreach($noLocs AS $noLoc){
+            $loc = DB::table("locations")->select(DB::raw("region, province, name_2 AS municipal, name_3 AS brgy"))->whereRaw("MbrWithin(GeomFromText(?), shape)", ['POINT('.$noLoc->lng.' '.$noLoc->lat.')'])->first();
+            DB::table('machines')
+            ->where('id', $noLoc->id)
+            ->update(['region' => $loc->region, 'province' => $loc->province, 'municipal' => $loc->municipal, 'brgy' => $loc->brgy]);
+        }
         return $this->sendResponse($noLoc, 'loc retrieved successfully.');
     }
 
