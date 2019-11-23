ca1a4d2a5600afcbc0ee5bd32640a4106495c4e1
Mark Loreto <markyctrigger27@gmail.com>
2019-11-23 17:36:06 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -371,7 +371,7 @@ class ApiController extends BaseController
             $recordsTotal = $machineFilter->count();
 
             if($export){
-                $machineFilter = $machineFilter->addSelect(DB::raw("m.client_id"));
+                $machineFilter = $machineFilter->addSelect(DB::raw("m.client_id, m.updated_at, m.accuracy, m.delivery, CONCAT('https://maps.google.com?q=', m.lat, ',' m.lng) AS `map`"));
             }
         }
             
