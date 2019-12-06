21762f208ab55025844ac8654212dd3b2603bceb
Mark Loreto <markyctrigger27@gmail.com>
2019-12-06 20:20:57 +0800

    ...

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -59,7 +59,7 @@ class ApiController extends BaseController
                 $f = Carbon::parse($record->csDate)->subDays(31);
 
                 $machinesCountToday = DB::table("machines AS m")
-                ->whereRaw("m.delivery = DATE_FORMAT('".$record->csDate."', '%a') AND m.created_at <= DATE('".$end."') AND m.staff_id = '".$dealerId."' AND id IN (SELECT machine_id FROM callsheets WHERE machine_id = m.id AND created_at >= '".$f->toDateTimeString()."' AND created_at <= '".$end."' LIMIT 1)")
+                ->whereRaw("m.delivery = DATE_FORMAT('".$record->csDate."', '%a') AND m.created_at <= DATE('".$end."') AND m.staff_id = '".$dealerId."' AND id IN (SELECT machine_id FROM callsheets WHERE machine_id = m.id)")
                 ->count();
 
                 $record->machinesCountToday = $machinesCountToday;
