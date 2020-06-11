d27b5a5b85dadc950f8df4657a35b882ac5992bc
Mark Loreto <markyctrigger27@gmail.com>
2020-06-12 02:55:58 +0800

    ...

--- app/Http/Controllers/API/PHPController.php
+++ app/Http/Controllers/API/PHPController.php
@@ -151,7 +151,7 @@ class PHPController extends BaseController
 
     public function ETinda(Request $request){
         $data = $request->all();
-        //$challenge = $data["hub.challenge"];
-        return response($data, 200);
+        $challenge = $data["hub_challenge"];
+        return response($challenge, 200);
     }
 }
