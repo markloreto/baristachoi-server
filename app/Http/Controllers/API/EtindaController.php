d237a6d02d9992dbb7df1702f31ae1aff5373b76
Mark Loreto <markyctrigger27@gmail.com>
2020-06-20 20:42:02 +0800

    ...

---
+++ app/Http/Controllers/API/EtindaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Hash;
+use DB;
+use Validator;
+use Intervention\Image\ImageManagerStatic as Image;
+use App\Services\PayUService\Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Stream\Stream;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Collection;
+use OneSignal;
+
+use Rap2hpoutre\FastExcel\FastExcel;
+
+use App\Exports\MachinesExport;
+use App\Exports\CallsheetsExport;
+use App\Exports\ClientsExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
+
+//
+class EtindaController extends BaseController
+{
+    
+}
