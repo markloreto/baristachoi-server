0fc22ad44fd90bd01ceff5e9330f5eae785edbf5
Mark Loreto <markyctrigger27@gmail.com>
2020-07-24 22:23:44 +0800

    ...

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -174,7 +174,7 @@ class EtindaController extends BaseController
         $categoryId = $data["categoryId"];
         
         $records = DB::table("pabile_products as pp")
-        ->select(DB::raw('pp.*, (SELECT COUNT(id) FROM pabile_inventories pi WHERE pi.product_id = pp.id AND pi.order_id IS NULL) AS inventory, (SELECT value FROM pabile_product_specs WHERE `key` = 6 AND product_id = pp.id) AS brand, (SELECT value FROM pabile_product_specs WHERE `key` = 1 AND product_id = pp.id) AS weight, (SELECT value FROM pabile_product_specs WHERE `key` = 2 AND product_id = pp.id) AS `color`'))->where("pp.category_id", $categoryId)->get();
+        ->select(DB::raw('pp.*, (SELECT COUNT(id) FROM pabile_inventories pi WHERE pi.product_id = pp.id AND pi.order_id IS NULL) AS inventory, (SELECT value FROM pabile_product_specs WHERE `key` = 6 AND product_id = pp.id) AS brand, (SELECT value FROM pabile_product_specs WHERE `key` = 1 AND product_id = pp.id) AS weight, (SELECT value FROM pabile_product_specs WHERE `key` = 2 AND product_id = pp.id) AS `color`, (SELECT thumbnail FROM pabile_product_photos WHERE product_id = pp.id AND `primary` = 1) AS `thumbnail`'))->where("pp.category_id", $categoryId)->get();
         return $this->sendResponse($records, 'getProducts');
     }
 
