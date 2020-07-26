6785911eb98f0521b2dd4c9ce7f20419c9b91ce4
Mark Loreto <markyctrigger27@gmail.com>
2020-07-26 22:19:38 +0800

    .

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -601,7 +601,7 @@ class EtindaController extends BaseController
                             "buttons" => array(
                                 0 => array(
                                     "type" => "web_url",
-                                    "url" => "https://pabile-e.web.app",
+                                    "url" => "https://pabile-e.firebaseapp.com/",
                                     "title" => "Visit"
                                 )
                             )
