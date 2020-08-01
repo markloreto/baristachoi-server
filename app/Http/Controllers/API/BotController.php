60b20e86fb1a842bab11c32decc1e20b0458e613
Mark Loreto <markyctrigger27@gmail.com>
2020-08-01 23:41:14 +0800

    .

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -40,7 +40,6 @@ class BotController extends BaseController
         ->where(["pp.id", $rec->product_id])
         ->join('pabile_product_categories AS ppc', 'pp.category_id', '=', 'ppc.id')
         ->select(DB::raw('pp.*, ppc.name AS category_name, (SELECT COUNT(id) FROM pabile_inventories pi WHERE pi.product_id = pp.id AND pi.order_id IS NULL) AS inventory, (SELECT value FROM pabile_product_specs WHERE `key` = 6 AND product_id = pp.id) AS brand, (SELECT value FROM pabile_product_specs WHERE `key` = 1 AND product_id = pp.id) AS weight, (SELECT value FROM pabile_product_specs WHERE `key` = 2 AND product_id = pp.id) AS `color`, (SELECT value FROM pabile_product_specs WHERE `key` = 5 AND product_id = pp.id) AS `flavor`, (SELECT value FROM pabile_product_specs WHERE `key` = 9 AND product_id = pp.id) AS `size`, (SELECT thumbnail FROM pabile_product_photos WHERE product_id = pp.id AND `primary` = 1) AS `thumbnail`'))
-        ->limit(10)
         ->first();
 
         $d->qty = $rec->qty;
