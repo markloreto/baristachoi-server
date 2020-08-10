4318eae99ac7d94c58effc9f751f0fb93fb318d8
Mark Loreto <markyctrigger27@gmail.com>
2020-08-10 22:07:52 +0800

    .

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -287,9 +287,9 @@ class EtindaController extends BaseController
         $records = DB::table("pabile_products as pp")
         ->select(DB::raw('pp.*, (SELECT COUNT(id) FROM pabile_inventories pi WHERE pi.product_id = pp.id AND pi.order_id IS NULL) AS inventory, (SELECT value FROM pabile_product_specs WHERE `key` = 6 AND product_id = pp.id) AS brand, (SELECT value FROM pabile_product_specs WHERE `key` = 1 AND product_id = pp.id) AS weight, (SELECT value FROM pabile_product_specs WHERE `key` = 2 AND product_id = pp.id) AS `color`, (SELECT thumbnail FROM pabile_product_photos WHERE product_id = pp.id AND `primary` = 1) AS `thumbnail`, (SELECT COUNT(*) FROM pabile_product_photos WHERE product_id = pp.id) AS `numPhotos`'));
         if($categoryId != 0){
-            $records->where("pp.category_id", $categoryId)->get();
+            $records = $records->where("pp.category_id", $categoryId)->get();
         }else{
-            $records->having('numPhotos', '=', 0)->get();
+            $records = $records->having('numPhotos', '=', 0)->get();
         }
         //->having('mySold', '!=', 0)
         
