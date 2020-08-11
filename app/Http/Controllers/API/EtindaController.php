8e8b2bab5caf32058f3db9aca56bcd8f4c88c2da
Mark Loreto <markyctrigger27@gmail.com>
2020-08-11 11:21:37 +0800

    .

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -332,7 +332,7 @@ class EtindaController extends BaseController
         $product = DB::table("pabile_products as pp")->select(DB::raw('pp.*, (SELECT parent_id FROM pabile_product_categories WHERE id = pp.category_id) AS parent_id, (SELECT name FROM pabile_product_categories WHERE id = pp.category_id) AS category_name'))->where("pp.id", $productId)->first();
         $specs = DB::table("pabile_product_specs as pps")->select(DB::raw('pps.*, (SELECT name FROM pabile_spec_keys WHERE id = pps.key) AS keyName'))->where("pps.product_id", $productId)->get();
 
-        if(false){
+        if($isDesktop){
             $photos = [];
         }else{
             $photos = DB::table("pabile_product_photos")->where("product_id", $productId)->get();
