53dc98942ca04329c0f1c4a47f695733aa2a13f9
Mark Loreto <markyctrigger27@gmail.com>
2020-08-12 11:05:26 +0800

    .

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -604,12 +604,12 @@ class EtindaController extends BaseController
         $records = DB::table("pabile_clients")->where("prefix_id", null)->get();
 
         foreach($records as $record){
-            $uMobilePrefix = substr($record->mobile, 0, 4);
+            $uMobilePrefix = intval(substr($record->mobile, 0, 4));
 
             $prefixRec = DB::table("pabile_mobile_prefixes")->where("prefix", $uMobilePrefix)->first();
             
             if($prefixRec == null){
-                $uMobilePrefix = substr($record->mobile, 0, 3);
+                $uMobilePrefix = intval(substr($record->mobile, 0, 3));
 
                 $prefixRec = DB::table("pabile_mobile_prefixes")->where("prefix", $uMobilePrefix)->first();
             }
