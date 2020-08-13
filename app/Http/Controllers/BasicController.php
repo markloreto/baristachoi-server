175fbb57f861fd8210d7e1e8f80a307307ed9856
Mark Loreto <markyctrigger27@gmail.com>
2020-08-13 11:23:41 +0800

    .

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -25,7 +25,7 @@ class BasicController extends Controller
         $path = storage_path("app/pabile/" . $photo);
         $maskPath = storage_path("app/public/mask.png");
         $img = Image::make($path);
-        $img->resize(null, 260, function ($constraint) {
+        $img->resize(500, null, function ($constraint) {
             $constraint->aspectRatio();
         });
         $w = $img->width();
