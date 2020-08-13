75cc1e4e3a70efeda0928a4628cddb6e0ba3233d
Mark Loreto <markyctrigger27@gmail.com>
2020-08-13 21:01:09 +0800

    .

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -287,7 +287,7 @@ class BotController extends BaseController
                     ]
                   }
                 }
-              },
+              }
             ]
           }', true);
     
