0a5745ff9f4c93ca5e323c3095c9322da9a34072
Mark Loreto <markyctrigger27@gmail.com>
2020-08-13 13:05:02 +0800

    .

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -39,20 +39,20 @@ class BasicController extends Controller
         $peso = Image::make($pesoPath);
         $img->insert($pesoPath, "top-left", 0, 8);
 
+        $img->text($p->price, 48, 58, function($font) {
+            $fontPath = storage_path("app/public/Roboto-Bold.ttf");
+            $font->size(42);
+            $font->color("#ffffff");
+            $font->file($fontPath);
+        });
+
         $img->text($p->price, 50, 60, function($font) {
             $fontPath = storage_path("app/public/Roboto-Bold.ttf");
-            $font->size(48);
+            $font->size(42);
             $font->color("#464646");
             $font->file($fontPath);
         });
 
-        $img->text($p->price, 48, 58, function($font) {
-            $fontPath = storage_path("app/public/Roboto-Bold.ttf");
-            $font->size(48);
-            $font->color("#ffffff");
-            $font->file($fontPath);
-        });
- 
         return $img->response();
     }
 
