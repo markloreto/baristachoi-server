f92541ce5e76fa1f574230eded7b9dcaeb05a393
Mark Loreto <markyctrigger27@gmail.com>
2020-08-17 09:13:54 +0800

    .

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -59,7 +59,7 @@ class BasicController extends Controller
             $smallPeso = Image::make($smallPesoPath);
             $img->insert($smallPesoPath, "top-left", 4, 75);
 
-            $img->text($p->previous_price, 25, 75, function($font) {
+            $img->text($p->previous_price, 30, 90, function($font) {
                 $fontPath = storage_path("app/public/BPtypewriteStrikethrough.ttf");
                 $font->size(14);
                 $font->color("#ffffff");
