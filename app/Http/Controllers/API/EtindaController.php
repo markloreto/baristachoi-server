2459f14c565b4d3e10477e1e028e84ea7d5d15bf
Mark Loreto <markyctrigger27@gmail.com>
2020-08-19 11:27:11 +0800

    .

--- app/Http/Controllers/API/EtindaController.php
+++ app/Http/Controllers/API/EtindaController.php
@@ -662,12 +662,13 @@ class EtindaController extends BaseController
 
         $records = DB::table("pabile_inventories as pi")->select(DB::raw('pi.purchase_id, pi.product_id, (SELECT DATE(created_at) FROM pabile_purchases WHERE id = pi.purchase_id) AS `Date`, (SELECT name FROM pabile_products WHERE id = pi.product_id) AS `Product Name`, pi.cost AS `Cost`, (SELECT COUNT(id) FROM pabile_inventories WHERE purchase_id = pi.purchase_id) AS `Loaded`, COUNT(pi.id) AS `Remaining`'))
         ->join('pabile_products as pp', 'pi.product_id', '=', 'pp.id')
-        ->whereRaw('pi.order_id IS NULL OR pp.virtual_cost IS NOT NULL OR pp.virtual_cost != 0')
+        ->whereRaw('pi.order_id IS NULL')
         ->orderBy("pi.purchase_id", "desc")
         ->orderBy("Date", "desc")
         ->groupBy("pi.purchase_id", "pi.product_id", "pi.cost")
         ->limit(20)
         ->offset($offset)
+        ->havingRaw('pp.virtual_cost IS NOT NULL OR pp.virtual_cost != 0')
         ->get();
 
         return $this->sendResponse($records, 'getPurchases');
