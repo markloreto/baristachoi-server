96e5124977b033a80f3f1845ae1ab2b848fd4cca
Mark Loreto <markyctrigger27@gmail.com>
2020-08-23 00:24:09 +0800

    .

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -37,7 +37,8 @@ class BotController extends BaseController
           {"text": "platform '.$agent->platform().'"},
           {"text": "browser '.$agent->browser().'"},
           {"text": "isDesktop '.$agent->isDesktop().'"},
-          {"text": "isRobot '.$agent->isRobot().'"}
+          {"text": "isRobot '.$agent->isRobot().'"},
+          {"text": "browser '.$agent->browser().'"}
         ]
        }', true);
 
