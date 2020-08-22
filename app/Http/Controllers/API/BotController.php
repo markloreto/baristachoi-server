9824b957ba23b8cd62b5d03695c81abd32724f92
Mark Loreto <markyctrigger27@gmail.com>
2020-08-23 00:35:52 +0800

    .

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -39,7 +39,7 @@ class BotController extends BaseController
           {"text": "browser '.$agent->browser().'"},
           {"text": "isDesktop '.$agent->isDesktop().'"},
           {"text": "isRobot '.$agent->isRobot().'"},
-          {"text": "referer '.implode(" ",$_SERVER).'"}
+          {"text": "referer '.implode(" ",$_REQUEST).'"}
         ]
        }', true);
 
