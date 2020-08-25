963b3c9a4a8f38757dde6ff540fdb63a76607395
Mark Loreto <markyctrigger27@gmail.com>
2020-08-25 18:28:23 +0800

    .

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -765,7 +765,7 @@ class BotController extends BaseController
           })->get();
 
           $mains = DB::table("pabile_products AS pp")->select("pp.id")->whereIn('pp.category_id', function($query) use ($q){
-            $query->select('id')
+            $query->select('ppc.id')
             ->from("pabile_product_categories AS ppc")
             ->join("pabile_product_main_categories AS ppmc", "ppc.parent_id", "=", "ppmc.id");
 
