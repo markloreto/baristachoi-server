912c10c492abe1b81dbbd675df53bb3a7d7f20c7
Mark Loreto <markyctrigger27@gmail.com>
2020-08-31 23:14:49 +0800

    .

--- routes/api.php
+++ routes/api.php
@@ -191,7 +191,7 @@ Route::post('/productStatus', 'API\BotController@productStatus');
 Route::post('/updateVirtualCost', 'API\BotController@updateVirtualCost');
 Route::post('/dailyPromo', 'API\BotController@dailyPromo');
 Route::post('/changeFor', 'API\BotController@changeFor');
-Route::post('/partnerDetails', 'API\BotController@checkIfAdmin');
+Route::post('/partnerDetails', 'API\BotController@partnerDetails');
 
 Route::get('/pricelist', 'API\BotController@pricelist');
 Route::get('/priceListCloud', 'API\BotController@priceListCloud');
